fa0553057ec236f21f242069fa2643728ff7bb88
Илья Петров <brahmagupta@tuta.io Primary Verified>
2025-09-22 22:18:11 +0300

    build: add empty tests

--- src/script.test.js
+++ src/script.test.js
@@ -1,2 +1,12 @@
-//Нужно начинать всегда с тестов
-test("", () => {});
+//Нужно начинать всегда с тестов ?
+test("Getting a relevant response from geojs", () => {});
+
+test("Check, that h3 element exist", () => {});
+test("Check, that input element exist", () => {});
+test("Check, that button element exist", () => {});
+
+test("Check, that ul element existed correctly", () => {});
+
+test("Check button behavior", () => {}); //Тестируем разные сценарии в одном тесте?
+
+//тест на то, что после введения значения и нажатию кнопки получаем необходимые данные
