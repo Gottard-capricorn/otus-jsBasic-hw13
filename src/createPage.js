db6a620385d2084ef75e5b4041d85771bee488e6
Илья Петров <brahmagupta@tuta.io Primary Verified>
2025-09-28 11:56:36 +0300

    refactor: clean up comments and update text content

--- src/createPage.js
+++ src/createPage.js
@@ -1,21 +1,13 @@
 import "./style.css";
 import { buttonBehavior, createElements } from "./utils.js";
 
-//Использовать стиль из кирупы
-
 createElements.createH3();
 createElements.createInput();
 createElements.createButton();
 createElements.createUl();
 
-document.querySelector("h3").textContent = "Данные о вашем местоположении:";
+document.querySelector("h3").textContent = "Данные о местоположении:";
 
 const button = document.querySelector("button");
 button.textContent = "click";
 button.addEventListener("click", buttonBehavior);
-
-//Если поле пустое и нажать кнопку, то данные о текущем местоположении
-//навесить на поведение кнопки, проверку:
-//1) Поле пустое? Если да, то текущее местоположение
-//Добавить логику по очищению поля
-//При двойном клике ошибка. Обработать в тесте
