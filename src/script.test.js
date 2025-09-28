848534a82401b1781b11ac68a510bb4ebb9b51b0
Илья Петров <brahmagupta@tuta.io Primary Verified>
2025-09-28 11:57:23 +0300

    test: add fetch-mock setup and expand test structure

--- src/script.test.js
+++ src/script.test.js
@@ -1,4 +1,6 @@
-//Нужно начинать всегда с тестов ?
+import { getInformation } from "./utils";
+import fetchMock from "fetch-mock";
+
 test("Getting a relevant response from geojs", () => {});
 
 test("Check, that h3 element exist", () => {});
@@ -8,5 +10,19 @@ test("Check, that button element exist", () => {});
 test("Check, that ul element existed correctly", () => {});
 
 test("Check button behavior", () => {}); //Тестируем разные сценарии в одном тесте?
+test("Verifiction data", () => {}); //тест на то, что после введения значения и нажатию кнопки получаем необходимые данные
+test("empty page", () => {});
+test("empty page multiple", () => {}); //множественное нажатие на клик, проверка
+test("fetch", async () => {
+  //   fetchMock.mockGlobal().route("https://get.geojs.io/v1/ip/geo.json", 200);
+  // const getGeoResult = getInformation.getGeo();
+  //   const result = await fetch("https://get.geojs.io/v1/ip/geo.json");
+  // const getGeoResultLength = Object.keys(getGeoResult).length;
+  // expect(typeof getGeoResult).toBe('object');
+  // expect(getGeoResultLength).toBe(13);
+}); //надо замокать fetch
 
-//тест на то, что после введения значения и нажатию кнопки получаем необходимые данные
+// Понять логику работы скрипта с codeSandbox. Добавить в repomix, а потом в нейронку на обработку
+// Добавить todo плагин
+// Посмотреть ещё раз тот плагин анимирующий
+// Использовать стиль из кирупы
